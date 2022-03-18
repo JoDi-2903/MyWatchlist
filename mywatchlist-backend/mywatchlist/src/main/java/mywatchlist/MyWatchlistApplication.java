25445f920784a78943668510e1a048368d37fc47
Benjamin BB <benjamin.bb@outlook.de>
2022-03-18 19:38:49 +0100

    init SpringBoot project

---
+++ mywatchlist-backend/mywatchlist/src/main/java/mywatchlist/MyWatchlistApplication.java
@@ -0,0 +1,13 @@
+package mywatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWatchlistApplication.class, args);
+	}
+
+}
---
+++ mywatchlist-backend/mywatchlist/src/test/java/mywatchlist/MyWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package mywatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
