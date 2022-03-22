128cb707d8ba3939aa84c3284ba4533903cd68e0
Patr1ick <padi.geiger@gmail.com>
2022-03-22 16:20:13 +0100

    Add React Router

---
+++ mywatchlist-frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+
+export default class LoginPage extends Component {
+    render(){
+        return(
+            <h1 className="text-3xl">Login</h1>
+        )
+    }
+}
---
+++ mywatchlist-frontend/src/pages/StartPage/StartPage.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+interface StartPageProps{
+
+}
+
+export default class StartPage extends Component<StartPageProps>{
+    render(){
+        return(
+            <h1 className="mt-5 ml-10 text-3xl">Hello World</h1>
+        )
+    }
+}
