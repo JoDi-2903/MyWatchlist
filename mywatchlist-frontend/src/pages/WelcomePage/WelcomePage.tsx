d9de53313093282ac95db5c279ecd2b7b2c00705
JoDi-2903 <jonathan.diebel@web.de>
2022-04-15 01:27:59 +0200

    prevent scrolling and hide scrollbar on WelcomePage

--- mywatchlist-frontend/src/pages/WelcomePage/WelcomePage.tsx
+++ mywatchlist-frontend/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,25 +1,15 @@
 import { Component } from "react";
+import disableScroll from 'disable-scroll';
 
+
+// prevent scrolling and hide scrollbar
+disableScroll.on();
+document.body.style.overflow = 'hidden';
 
 const pageLimit: React.CSSProperties = {
     height: '100vh',
     overflow: 'hidden'
 };
-
-// Deactivate buttons for scrolling
-window.addEventListener("keydown", function(e) {
-    // space, page up, page down and arrow keys:
-    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-        e.preventDefault();
-    }
-}, false);
-
-// Deactivate scroll with mousewheel click
-window.addEventListener("mousedown", function(e) {
-    if (e.which === 2) {
-      e.preventDefault();
-    }
-  });
 
 export default class WelcomePage extends Component {
     render() {
