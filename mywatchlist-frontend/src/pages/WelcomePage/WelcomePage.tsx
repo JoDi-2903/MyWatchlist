1a6b0e44cdd8489a48a3e5440d8d17bbd2cb9587
JoDi-2903 <jonathan.diebel@web.de>
2022-04-15 00:02:16 +0200

    deactivated page scrolling on up-/down keys

--- mywatchlist-frontend/src/pages/WelcomePage/WelcomePage.tsx
+++ mywatchlist-frontend/src/pages/WelcomePage/WelcomePage.tsx
@@ -6,6 +6,13 @@ const pageLimit: React.CSSProperties = {
     overflow: 'hidden'
 };
 
+window.addEventListener("keydown", function(e) {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 export default class WelcomePage extends Component {
     render() {
         return (
