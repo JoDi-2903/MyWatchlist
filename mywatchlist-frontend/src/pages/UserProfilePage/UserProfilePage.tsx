e9f84fef7c14d51a59a285bf1f16dc2b86197dd6
Patr1ick <padi.geiger@gmail.com>
2022-04-25 00:07:57 +0200

    Update UserProfilePage and SettingsPage

--- mywatchlist-frontend/src/pages/UserProfilePage/UserProfilePage.tsx
+++ mywatchlist-frontend/src/pages/UserProfilePage/UserProfilePage.tsx
@@ -1,14 +1,22 @@
 import {useParams} from "react-router-dom";
 import UserProfile from "../../components/UserProfile/UserProfile";
+import { JWTContext } from "../../security/JWTContext";
 
 const UserProfilePage = () => {
     
     const params = useParams();
 
     return (
-        <div className="flex justify-center mx-auto my-10 p-10 w-3/4">
-            <UserProfile username={params.id as string} />
-        </div>
+        <JWTContext.Consumer>
+            {
+                ({jwtInfo, changeJWT}) => (
+                    <div className="flex justify-center mx-auto my-10 p-10 w-3/4">
+                        <UserProfile username={params.id as string} jwtInfo={jwtInfo} />
+                    </div>
+                )
+            }
+
+        </JWTContext.Consumer>
     )
 }
 
