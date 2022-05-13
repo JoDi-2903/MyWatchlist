732a71ebd389846ce947017795f4d5fc0294ab91
JoDi-2903 <jonathan.diebel@web.de>
2022-05-13 02:25:58 +0200

    added movie poster

--- mywatchlist-frontend/src/pages/MoviePage/MoviePage.tsx
+++ mywatchlist-frontend/src/pages/MoviePage/MoviePage.tsx
@@ -31,12 +31,19 @@ export default class MoviePage extends Component<{}, { movieID: number, poster: 
             <div>
                 <div className="bg-gradient-to-tl from-black via-transparent to-transparent h-96 w-full bg-cover bg-center relative">
                     <img src={this.state.backdrop} className="w-full h-full object-cover absolute mix-blend-overlay" />
-                    <div className="p-24">
-                        <h1 className="text-white text-6xl font-bold">This is the movie title</h1>
-                        <h2 className="text-white text-3xl font light mt-5">Some more space for subtitles.</h2>
+                    <div className="grid-background absolute inset-0 p-2 grid grid-cols-12 gap-0">
+                        <div className="col-span-1"></div>
+                        <div className="col-span-2">
+                            <img src={this.state.poster} className="h-auto w-auto object-cover rounded-lg border-2 border-white shadow-2xl shadow-black" />
+                        </div>
+                        <div className="col-span-9">
+                            <div className="p-24 absolute">
+                                <h1 className="text-white text-6xl font-bold">This is the movie title</h1>
+                                <h2 className="text-white text-3xl font light mt-5">Some more space for subtitles.</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
             </div >
         );
     }
