ea2cc41fe9aafac85764455d7d8f64d3659fbe6e
JoDi-2903 <jonathan.diebel@web.de>
2022-05-13 11:58:47 +0200

    updated detail page

--- mywatchlist-frontend/src/pages/MoviePage/MoviePage.tsx
+++ mywatchlist-frontend/src/pages/MoviePage/MoviePage.tsx
@@ -1,10 +1,9 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieImages } from "../../api/API";
+import { getMovieImages, getMovieDetail } from "../../api/API";
 import { apiConfig } from "../../Config";
 
-
-export default class MoviePage extends Component<{}, { movieID: number, poster: string, backdrop: string }> {
+export default class MoviePage extends Component<{}, { movieID: number, poster: string, backdrop: string, original_title: string, release_date: string, tagline: string, genres: string[], runtime: number, adult: boolean}> {
 
     constructor(props) {
         super(props);
@@ -12,24 +11,38 @@ export default class MoviePage extends Component<{}, { movieID: number, poster: 
             movieID: 0,
             poster: "",
             backdrop: "",
+            original_title: "",
+            release_date: "",
+            tagline: "",
+            genres: [],
+            runtime: 0,
+            adult: false,
         };
     }
     async componentDidMount() {
         //const params = useParams();
         //var movieID = parseInt(params.id as string);
         var movieID = 675353;
+
+        var movieDetails = await getMovieDetail(movieID);
         var movieImages = await getMovieImages(movieID);
         var posters = movieImages.data.posters;
         var backdrops = movieImages.data.backdrops;
         this.setState({
             poster: apiConfig.originalImage(posters[0].file_path),
             backdrop: apiConfig.originalImage(backdrops[0].file_path),
+            release_date: movieDetails.data.release_date,
+            original_title: movieDetails.data.original_title,
+            tagline: movieDetails.data.tagline,
+            genres: movieDetails.data.genres,
+            runtime: movieDetails.data.runtime,
+            adult: movieDetails.data.adult,
         });
     }
     render() {
         return (
             <div>
-                <div className="bg-gradient-to-tl from-black via-transparent to-transparent h-96 w-full bg-cover bg-center relative">
+                <div className="bg-gradient-to-tl from-black via-dark_bg to-dark_bg h-96 w-full bg-cover bg-center relative">
                     <img src={this.state.backdrop} className="w-full h-full object-cover absolute mix-blend-overlay" />
                     <div className="grid-background absolute inset-0 p-2 grid grid-cols-12 gap-0">
                         <div className="col-span-1"></div>
@@ -38,8 +51,12 @@ export default class MoviePage extends Component<{}, { movieID: number, poster: 
                         </div>
                         <div className="col-span-9">
                             <div className="p-24 absolute">
-                                <h1 className="text-white text-6xl font-bold">This is the movie title</h1>
-                                <h2 className="text-white text-3xl font light mt-5">Some more space for subtitles.</h2>
+                                <h1 className="text-white text-6xl font-bold">{this.state.original_title}
+                                    <span className="text-primary italic font-medium"> ({this.state.release_date.substring(0,4)})</span>
+                                </h1>
+                                <h2 className="text-white text-4xl font-light mt-5 italic ">{this.state.tagline}</h2>
+                                <h4 className="text-white text-xl font-ligth mt-20"> Adult: {this.state.adult} • Genres • Rating: ★★★★☆ • Runtime: {this.state.runtime} min</h4>
+
                             </div>
                         </div>
                     </div>
